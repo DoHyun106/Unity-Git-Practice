08fa09bf202449fc5f4624407dd279d6e102cc21
Hyun <brian_star@naver.com>
2022-03-11 17:18:40 +0900

    20220311.01 ver

---
+++ Assets/Scriptes/UserSetScript.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetScript : MonoBehaviour
+{
+    // 종료 안내 오브젝트
+    public GameObject escapeMassage, escapeYes, escapeNo;
+    // 설정 패널 오브젝트
+    public GameObject userSetPanel;
+    // 오디오 믹서
+    public AudioMixer audioMixer;
+    // 오디오 슬라이더 컨트롤 오브젝트
+    public Slider sliderMaster, sliderBGM, sliderSFX, sliderMenuSFX;
+
+    // UserSet Funtion
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            openUserSetPanel();
+        }
+    }
+    public void openUserSetPanel()
+    {
+        Time.timeScale = 0f;
+        userSetPanel.SetActive(true);
+    }
+    public void closeUserSetPanel()
+    {
+        userSetPanel.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    // Audio Control Funtion
+    public void audioMasterControl()
+    {
+        float volume = sliderMaster.value;
+
+        if(volume == -40f)  // 슬라이더 값이 -40 이면 소리값을 -80로 설정 (음소거)
+        {
+            audioMixer.SetFloat("Master", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("Master", volume);
+        }
+    }
+    public void audioBGMControl()
+    {
+        float volume = sliderBGM.value;
+
+        if(volume == -40f)
+        {
+            audioMixer.SetFloat("BGM", -80);
+        }
+        else
+        {
+            audioMixer.SetFloat("BGM", volume);
+        }
+    }
+
+    // Escape Funtion
+    public void openEscapeMassage()
+    {
+        escapeMassage.SetActive(true);
+    }
+    public void executeEscape()
+    {
+        Application.Quit();
+    }
+
+    public void unexcuteEscape()
+    {
+        escapeMassage.SetActive(false);
+    }
+}
